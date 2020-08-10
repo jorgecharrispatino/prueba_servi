a0030609f6839a46e9ba6d54bf7a65b94337869d
jorgecharrispatino <jorge.charrispatino@gmail.com>
2020-08-09 20:21:24 -0500

    guardar final

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import include, path
 from polls.routes import router
 from django.conf.urls import url
+import polls.views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls')),
+    url(r"cantidad-votantes", v.CantidadVotantes.as_view()),
+    url(r"volantes-lider", v.VolantesLider.as_view()),
+    url(r"votantes-municipios", v.VotantesMunicipios.as_view()),
+    url(r"votantes-mesas", v.VotantesMesas.as_view()),
 ]
